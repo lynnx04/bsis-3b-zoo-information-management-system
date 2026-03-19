0e1142970af63c7536e2eee43436005908e6855b
stephaniekeyk <c20234745.figueroa@csav.edu.ph>
2026-03-19 12:44:05 +0800

    wdbgv

---
+++ app/Livewire/Pages/Public/SinglePage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages\Public;
+
+use Livewire\Component;
+
+class SinglePage extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.public.single-page');
+    }
+}
